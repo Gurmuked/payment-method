3b52fdf6db3908cc2b31b4e211b2bc989c08a90b
Gurmesa <gurmesakedir96@gmail.com>
2025-04-30 00:57:35 +0300

    customize the css and add breaking means resposiveness

--- payment-method/tailwind.config.js
+++ payment-method/tailwind.config.js
@@ -4,8 +4,27 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimwhite: "rgba(255, 255, 255, 0.7)",
+        dimblue: "rgba(9, 151, 124, 0.1)",
+        },
+      fontFamily: {
+        poppins: ["poppins", "sans-serif"]
+        },
+    },
+     screens: {
+      xs: "480px",
+      xs: "620px",
+      xs: "768px",
+      xs: "1060px",
+      xs: "1200px",
+      xs: "1700px"
+     },
   },
   plugins: [],
 }

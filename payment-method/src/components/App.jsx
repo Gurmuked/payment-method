162912cacf210347a13afa6e92db3b1491c0222e
Gurmesa <gurmesakedir96@gmail.com>
2025-04-30 00:22:22 +0300

    this is the next generation payment method front end published by tailwind and react js this all step up of file

---
+++ payment-method/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-4xl text-red-900 text-center'>Gurmesa</h1>
+    </>
+  )
+}
+
+export default App

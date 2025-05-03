e56bd5744f52410a982de0c497fd6b10c9a67112
Gurmesa <gurmesakedir96@gmail.com>
2025-05-04 00:25:36 +0300

    create all important components

---
+++ payment-method/src/App.jsx
@@ -0,0 +1,33 @@
+import styles from './style';
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
+
+const App = () => (
+    <div className='bg-black w-full overflow-hidden'>
+      <div classname={`${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+           <Navbar />
+        </div>
+      </div>
+      <div className={`bg-black ${styles.flexStart }`}>
+        <div className={`${styles.boxWidth}`}>
+            <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-black ${styles.paddingX} ${styles.flexStart }`}>
+        <div className={`${styles.boxWidth}`}>
+            <Stats />
+            <Business />
+            <Billing />
+            <CardDeal />
+            <Testimonials />
+            <Clients />
+            <CTA />
+            <Footer />
+        </div>
+      </div>
+
+    </div>
+  )
+
+export default App
---
+++ payment-method/src/components/index.js
@@ -0,0 +1,30 @@
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Button from "./Button";
+import GetStarted from "./GetStarted";
+import Feedback from "./Feedback";
+
+
+export {
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Clients,
+  CTA,
+  Testimonials,
+  Footer,
+  Navbar,
+  Button,
+  GetStarted,
+  Feedback,
+}

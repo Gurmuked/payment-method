443f64eccc387fc86decd4c858dc535ef93a1b70
Gurmesa <gurmesakedir96@gmail.com>
2025-05-06 00:09:45 +0300

    status of our company  or part next to hero section.

--- payment-method/src/components/Navbar.jsx
+++ payment-method/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants/index';
 
 const Navbar = () => {
-  
+
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
 
@@ -30,11 +30,13 @@ const Navbar = () => {
           className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle(!toggle)}
         />
+
         <div
           className={`${
             !toggle ? "hidden" : "flex"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
+
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
@@ -48,6 +50,7 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+
         </div>
       </div>
 
